a775ae2142eb16c203686707b3719d16f33ded1d
Vinícius Gabriel <viniciusgsa99@gmail.com>
2022-11-21 20:32:00 -0300

    Requisito 13 só falta 1 sub requisito para passar.

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Player1');
+const player2 = new Character('Player2');
+const player3 = new Character('Player3');
+
+console.log(player1.lifePoints);
+for (let i = 0; i < 10; i += 1) player1.levelUp();
+console.log(player1.lifePoints);
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => battle.fight());
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
